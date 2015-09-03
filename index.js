aa7b26ad29f1d1820a3a930a5dba8135ddcab6fd
Kenneth Liu <ksliu25@gmail.com>
2015-09-03 01:20:54 -0500

    add websocket ajax functionality

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var port = process.env.PORT || 3030;
 // app.get('/', function(req, res){
 //   res.send('Hello world');
 // });
+
 var users = []
 http.listen(port, function(){
   console.log('listening on *:' + port);
@@ -15,17 +16,25 @@ io.on('connection', function(socket){
   console.log('a user connected');
   users.push("user")
   console.log("current count: " + users.length)
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-    //## only returns first line, makes sense
-    // console.log('message: ' + msg);
-    // console.log("no emit?")
-  });
 
-  socket.on('ajaxsend', function(msg){
-  	console.log(msg)
-  	io.emit('ajaxreturn', "Ajax Returned")
+  socket.on('tellGuestsToUpdate', function(id){
+    console.log("guest")
+    io.emit('guestUpdatePlaylist', id)
   })
+  
+
+
+  // socket.on('chat message', function(msg){
+  //   io.emit('chat message', msg);
+  //   //## only returns first line, makes sense
+  //   // console.log('message: ' + msg);
+  //   // console.log("no emit?")
+  // });
+
+  // socket.on('ajaxsend', function(msg){
+  // 	console.log(msg)
+  // 	io.emit('ajaxreturn', "Ajax Returned")
+  // })
 
 
 
